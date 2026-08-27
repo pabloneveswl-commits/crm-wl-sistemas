c21bb17ab747c304246e2c1031cde7212dc46989
pabloneveswl-commits <pabloneveswl@gmail.com>
2026-08-27 14:56:14 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL="https://gxfyeiqstfrnimzgkgvw.supabase.co";
+
+const SUPABASE_KEY="COLE_AQUI_SUA_ANON_KEY";
+
+const supabaseClient=window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
